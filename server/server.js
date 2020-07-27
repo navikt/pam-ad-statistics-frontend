37c5a31fa178642f8c4abe48d35e1bf050777959
Martin Wangen-Eriksen <martin.wangen-eriksen@nav.no>
2020-07-27 14:52:30 +0200

    change server file

--- server/server.js
+++ server/server.js
@@ -8,10 +8,9 @@ var ad = 'json_fil'
 
 server.set('port',port)
 
-
-server.get('/', function (req, res) {
-    res.send('I\'m running!');
-});
+const pathProperties = {
+    PAM_CONTEXT_PATH: '/ad'
+}
 
 server.use(cors({
     origin: 'http://localhost:3000'
@@ -20,7 +19,7 @@ server.use(cors({
 server.get('/ad/:id', function(req, res){
 
     const key = req.params.id //not real key
-    const api_url = `http://localhost:8080/ad/${key}`
+    const api_url = `http://localhost:8080${fasitProperties.PAM_CONTEXT_PATH}/${key}`
 
     fetch(api_url,)
         .then(res => res.json())
@@ -31,8 +30,8 @@ server.get('/', (req, res) => {
     res.redirect('/ad')
 });
 
-server.get('/ad/internal/isAlive', (req, res) => res.sendStatus(200));
-server.get('/ad/internal/isReady', (req, res) => res.sendStatus(200));
+server.get(`${pathProperties.PAM_CONTEXT_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
+server.get(`${pathProperties.PAM_CONTEXT_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
 
 
 
