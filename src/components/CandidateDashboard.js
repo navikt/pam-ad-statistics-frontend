8bf18202a708a271f27ea65ff6c818c34c7f8fab
catriont <catriona.torklep@gmail.com>
2020-07-28 16:24:31 +0200

    create layout for demo of both company and candidate dashboards

---
+++ src/components/CandidateDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CandidateSearchHits from './DashboardComponents/CandidateSearchHits';
+
+const CandidateDashboard = () => {
+
+    return (
+        <CandidateSearchHits/>
+    )
+
+};
+
+export default CandidateDashboard;
---
+++ src/components/DashboardComponents/CandidateSearchHits.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CandidateSearchHits = () => {
+
+    return (
+        <div> hei </div>
+    )
+
+}
+
+export default CandidateSearchHits;
