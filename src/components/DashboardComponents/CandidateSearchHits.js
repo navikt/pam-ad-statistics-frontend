0d24ad3d19840afca9715f5279ed745466b3df69
catriont <catriona.torklep@gmail.com>
2020-07-29 10:26:23 +0200

    Update text display on # of hits > 0

--- src/components/DashboardComponents/CandidateSearchHits.js
+++ src/components/DashboardComponents/CandidateSearchHits.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 const CandidateSearchHits = () => {
 
-    var hits = 0;
+    //var hits = 0;
 
-    // var hits = 3;
+    var hits = 3;
 
     return (
-        <div> {hits ? (hits) : (<>(Insert: ressurser hvor hvordan dukke opp i flere søk)</>)} </div>
+        <div> {hits ? 
+            <React.Fragment>
+                <div className = "text-component__top-left"> {hits} </div>
+                <div className = "text-component__bot-left"> forekomster i kandidatsøk de siste 2 ukene </div>
+            </React.Fragment> : 
+            (<>(Insert: ressurser hvor hvordan dukke opp i flere søk)</>)} </div>
     )
 
 }
