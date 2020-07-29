14907185eae7916c893f70f5a87a50934cb09fe0
Martin Wangen-Eriksen <martin.wangen-eriksen@nav.no>
2020-07-29 10:47:49 +0200

    add temporary solution to sort get calls by type

--- src/DataContext.js
+++ src/DataContext.js
@@ -6,7 +6,7 @@ export const DataContext = createContext({});
 export const DataProvider = props => {
 
     const [data, setData] = useState({
-        "pageViewsAd": 0,
+        "pageViews": 0,
         "pageViewsCandidate": 0,
         "averageTimeOnPage": [0
         ],
@@ -20,15 +20,26 @@ export const DataProvider = props => {
         ] 
     });
 
-    //const path = window.location.pathname.split("/").pop()
-    //keep for using when not utilizing local host
 
-    const path = 'd1508053-0982-401d-985d-e9660d2a0cbc'
+    const pathList = ('nav.no/stillinger/stilling/d1508053-0982-401d-985d-e9660d2a0cbc').split("/")
+    const id = pathList.pop()
+    var type = pathList.pop()
+
+    if(type === 'stilling'){
+        type = 'ad'
+    }else{
+        type = 'candidate'
+    }
+
+    console.log(id, type)
+
+    //window.location.pathname
+    //keep for using when not utilizing local host
 
     useEffect(() => {
     const fetchData = async () => {
         const result = await axios(
-        'http://localhost:9000/ad/' + path,
+        'http://localhost:9000/'+ type + '/' + id,
         );
         setData(result.data);
     };
--- src/components/DashboardComponents/TotalPageViews.js
+++ src/components/DashboardComponents/TotalPageViews.js
@@ -11,7 +11,7 @@ const TotalPageviews = () => {
 
     return(
         <div className = "text-component">
-            <div className = "text-component__top-left">{data.pageViewsAd}</div>
+            <div className = "text-component__top-left">{data.pageViews}</div>
             <div className = "text-component__bot-left">Klikk på annonsen</div>
         </div>
         
