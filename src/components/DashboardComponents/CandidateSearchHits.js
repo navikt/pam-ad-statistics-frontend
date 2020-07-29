0df2a32b388e6c97c11e2d608ba6d6338add9414
catriont <catriona.torklep@gmail.com>
2020-07-29 10:47:51 +0200

    Add link to article on # of hits = 0

--- src/components/DashboardComponents/CandidateSearchHits.js
+++ src/components/DashboardComponents/CandidateSearchHits.js
@@ -2,17 +2,23 @@ import React from 'react'
 
 const CandidateSearchHits = () => {
 
-    //var hits = 0;
+    var hits = 0;
 
-    var hits = 3;
+    //var hits = 3;
 
     return (
         <div> {hits ? 
             <React.Fragment>
                 <div className = "text-component__top-left"> {hits} </div>
                 <div className = "text-component__bot-left"> forekomster i kandidatsøk de siste 2 ukene </div>
-            </React.Fragment> : 
-            (<>(Insert: ressurser hvor hvordan dukke opp i flere søk)</>)} </div>
+            </React.Fragment> 
+            : 
+            <React.Fragment>
+                (Insert: ressurser hvor hvordan dukke opp i flere søk)
+                <a href = "https://arbeidsplassen.nav.no/slik-skriver-du-en-god-cv"> Slik skriver du en god cv </a>
+            </React.Fragment>
+            } 
+        </div>
     )
 
 }
