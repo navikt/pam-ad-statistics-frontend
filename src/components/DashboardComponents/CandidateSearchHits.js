fa01d6ee06100beacfce21dc7fe50d0b059eef38
catriont <catriona.torklep@gmail.com>
2020-07-30 09:30:14 +0200

    Change displayed info to weekly; show nothing on 0 hits

--- src/components/DashboardComponents/CandidateSearchHits.js
+++ src/components/DashboardComponents/CandidateSearchHits.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 const CandidateSearchHits = () => {
 
-    var hits = 0;
-
-    //var hits = 3;
+    var hits = 5;
 
     return (
         <div> {hits ? 
             <React.Fragment>
                 <div className = "text-component__top-left"> {hits} </div>
-                <div className = "text-component__bot-left"> forekomster i kandidatsøk de siste 2 ukene </div>
+                <div className = "text-component__bot-left"> forekomster i kandidatsøk denne uken </div>
             </React.Fragment> 
-            : 
-            <React.Fragment>
-                (Insert: ressurser hvor hvordan dukke opp i flere søk)
-                <a href = "https://arbeidsplassen.nav.no/slik-skriver-du-en-god-cv"> Slik skriver du en god cv </a>
-            </React.Fragment>
+            : null
             } 
         </div>
     )
 
 }
+/*
+            <React.Fragment>
+                <div>Tips til hvordan du kan dukke opp i flere kandidatsøk:</div>
+                <a href = "https://arbeidsplassen.nav.no/slik-skriver-du-en-god-cv"> Slik skriver du en god cv </a>
+            </React.Fragment>
+*/
 
 export default CandidateSearchHits;
