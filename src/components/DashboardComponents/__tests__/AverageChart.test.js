bcb5b9ec05370a732a4f138f622bdcb5fdb4d3d7
catriont <catriona.torklep@gmail.com>
2020-08-05 11:06:06 +0200

    add dummy test

---
+++ src/components/DashboardComponents/__tests__/AverageChart.test.js
@@ -0,0 +1,10 @@
+const { idText } = require("typescript")
+
+
+
+describe('<AverageChart/>', () => {
+    it('Calculates average time', () => {
+        const value = 2 + 2;
+        expect(value).toBeGreaterThan(3);
+    })
+});
