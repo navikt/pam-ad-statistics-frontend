6460e751bd0d0f969c1fe297c5fa64c16cae6c2e
Martin Wangen-Eriksen <martin.wangen-eriksen@nav.no>
2020-08-05 14:59:56 +0200

    make changes to other dashboard aswell

--- src/components/CompanyDashboard.js
+++ src/components/CompanyDashboard.js
@@ -3,29 +3,25 @@ import AverageChart from './DashboardComponents/AverageChart';
 import TotalPageViews from './DashboardComponents/TotalPageViews'
 import '../App.css';
 import ChartButton from './DashboardComponents/ChartButton';
-import { Provider } from 'react-redux';
-import {Store} from '../store/index';
 
 export const Dashboard = () => {
 
   
     return (
-        <Provider store={Store()}>
-            <div className = "dashboard">
+        <div className = "dashboard">
 
-                <div className = "text-components">
-                    
-                        <TotalPageViews/>
-                    
-                    
-                        <AverageChart/>
-                    
-                </div>
-
-                <ChartButton/>
-
+            <div className = "text-components">
+                
+                    <TotalPageViews/>
+                
+                
+                    <AverageChart/>
+                
             </div>
-        </Provider>
+
+            <ChartButton/>
+
+        </div>
     );
   }
   
